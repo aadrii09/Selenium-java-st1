05f631c2384a5d07853ced77adea2b71240121c7
aadrii09 <adriancastrobeiro@gmail.com>
2025-04-28 09:43:30 +0200

    hecho el login automatico

--- src/test/java/e2e/FirstTest.java
+++ src/test/java/e2e/FirstTest.java
@@ -1,6 +1,11 @@
 package e2e;
 
+import java.time.Duration;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstTest {
@@ -11,9 +16,24 @@ public class FirstTest {
         ChromeDriver web = new ChromeDriver();
         //Para maximizar la ventana del navegador
         web.manage().window().maximize(); 
-
+        web.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //Esperar 10 segundos a que cargue la página
         web.get("https://saucedemo.com");
 
+        //Para coger un elemento por su selector
+        WebElement usernameInput = web.findElement(By.cssSelector("input[data-test='username']"));
+        usernameInput.sendKeys("standard_user");
+
+        WebElement passwordInput = web.findElement(By.cssSelector("input[data-test='password']"));
+        passwordInput.sendKeys("secret_sauce");
+
+        WebElement loginButton = web.findElement(By.id(("login-button")));
+        loginButton.click();
+
+        WebElement productList = web.findElement(By.cssSelector("[data-test='inventory-list']"));
+        Boolean isProductListVisible = productList.isDisplayed();
+        Assertions.assertTrue(isProductListVisible);
+        System.out.println("TEST PASSED");
+        
         //Para cerrar el navegador al finalizar la prueba
         // web.quit();
     }
